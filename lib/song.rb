a41e381a8adafdaee13944021dbe4bc2a81a9a40
Hannah Malm <hannahemalm616@gmail.com>
2021-02-05 00:41:42 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -45,6 +45,7 @@ class Song
     self.all.find {|song| song.name == name}
   end 
   
+  #both methods with or in the middle 
   def self.find_or_create_by_name(name)
     self.find_by_name(name) || self.create_by_name(name)
   end 
